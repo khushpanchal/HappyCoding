b1cd16eb551a167e01e7f7741036e61e8dd51cbd
khushpanchal <khush.panchal123@gmail.com>
2024-03-15 13:09:46 +0530

    Add Kotlin Primitive

---
+++ app/src/main/java/com/khush/happycoding/kotlin/KotlinPrimitive.kt
@@ -0,0 +1,15 @@
+package com.khush.happycoding.kotlin
+
+class KotlinPrimitive {
+    private var a: Int = 1
+    private var b: Int? = 1
+
+    fun funcA() {
+        var c: Int? = 1
+    }
+
+    fun funB() {
+        var d: Int? = 1
+        d = null
+    }
+}
